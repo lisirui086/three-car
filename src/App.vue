a8dec252808b024ee26b27383bb841ffdd18d06f
William <lisirui086@gmail.com>
2023-05-06 22:32:50 +0800

    构建基础汽车模型

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,12 @@ import * as THREE from 'three'
 // 引入轨道控制器
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
+// 引入GLTF加载器
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
+
+// DRACOLoader
+import { DRACOLoader  } from 'three/examples/jsm/loaders/DRACOLoader'
+
 // 引入组合式API
 import { onMounted, reactive, ref, onUnmounted } from 'vue'
 
@@ -41,6 +47,46 @@ gridHelper.material.opacity = 0.2
 gridHelper.material.transparent = true
 scene.add(gridHelper)
 
+// 添加环境光
+/* const ambientLight = new THREE.AmbientLight({ color: 0xffffff, intensity: 1})
+scene.add(ambientLight) */
+
+// 添加平行光
+const light1 = new THREE.DirectionalLight(0xffffff, 0.5)
+light1.position.z = 10
+scene.add(light1)
+
+const light2 = new THREE.DirectionalLight(0xffffff, 1)
+light1.position.z = -10
+scene.add(light2)
+
+const light3 = new THREE.DirectionalLight(0xffffff, 1)
+light1.position.x = 10
+scene.add(light3)
+
+const light4 = new THREE.DirectionalLight(0xffffff, 1)
+light1.position.x = -10
+scene.add(light4)
+
+const light5 = new THREE.DirectionalLight(0xffffff, 1)
+light1.position.y = 10
+scene.add(light5)
+
+const light6 = new THREE.DirectionalLight(0xffffff, 1)
+light1.position.set(5, 10, 0)
+scene.add(light6)
+
+const light7 = new THREE.DirectionalLight(0xffffff, 0.5)
+light1.position.set(0, 10, 5)
+scene.add(light7)
+
+const light8 = new THREE.DirectionalLight(0xffffff, 0.5)
+light1.position.set(0, 10, -5)
+scene.add(light8)
+
+const light9 = new THREE.DirectionalLight(0xffffff, 0.5)
+light1.position.set(-5, 10, 0)
+scene.add(light9)
 
 // 渲染函数
 const renderFn = () => {
@@ -68,8 +114,25 @@ onMounted(() => {
   scene.background = new THREE.Color('#ccc')
   scene.environment = new THREE.Color('#ccc')
 
-  // 创建控制器
+  // 创建控制器 control已经在全局中声明了
   control = new OrbitControls(camera, canvas.value)
+  control.update()
+
+  // 创建汽车模型
+  const gltfLoader = new GLTFLoader()
+  const dracoLoader = new DRACOLoader()
+
+  dracoLoader.setDecoderPath( '/draco/gltf/' )
+  gltfLoader.setDRACOLoader( dracoLoader )
+
+  gltfLoader.load(
+    '/model/bmw01.glb',
+    (gltf) => {
+      const model = gltf.scene
+      model.scale.set(1.5,1.5,1.5)
+      scene.add(model)
+    }
+  )
 
   // 将侦听渲染器、相机宽高大小改变函数加到window
   window.addEventListener('resize', handlResize)
